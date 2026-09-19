6f9db4ab3b70efa579a4ea7546183f30d59b5ee9
ecomermix-sudo <ecomermix@gmail.com>
2026-09-19 19:18:25 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   SUPABASE_URL: 'https://gbpwnmxiqraqawonvlnu.supabase.co',
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdicHdubXhpcXJhcWF3b252bG51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0MjgxNjAsImV4cCI6MjEwMTAwNDE2MH0.wapFuPZXawp64H7y7vwRtjvFGUup7kUaswfH9pxX844',
-  PRODOTTO_SLUG: 'floorbot-pro',
+  PRODOTTO_SLUG: 'test-3',
   STRIPE_PUBLISHABLE_KEY: 'pk_test_placeholder',
 };
 
